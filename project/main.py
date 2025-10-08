539f14f94fc702d88192359197170fd16b9d7add
TayyabShoukat <tayyabshoukat1995@gmail.com>
2025-10-08 15:54:46 +0500

    add packages

---
+++ project/main.py
@@ -0,0 +1,6 @@
+from package.calculator import sum
+
+
+sum(5,10)
+sum(50,100,40)
+sum()
---
+++ project/package/calculator.py
@@ -0,0 +1,2 @@
+def sum(firstNumber=0,secondNumber=0,thirdNumber=5):
+    print("Sum is",firstNumber+secondNumber+thirdNumber)
