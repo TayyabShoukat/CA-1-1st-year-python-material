5b572c4a51d73d68d5ea1c75ed679dbca4feb5a1
TayyabShoukat <tayyabshoukat1995@gmail.com>
2025-10-24 23:13:22 +0500

    add

--- project/main.py
+++ project/main.py
@@ -2,9 +2,12 @@ from package.studentInfo import show
 from package.calculator import sum,multiply
 from package.square import square
 
-
-if __name__== "__main__":
+def main():
     show()
     print(sum(10,20,30)+100)
     multiply(10,20)
     print("Square is:",square(10)+2)
+
+
+if __name__== "__main__":
+    main()
